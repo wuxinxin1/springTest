54fe35b5852a7c38300cfadc9f56b71aaae51ed5
wuxinxin <18770153013@163.com>
2020-03-11 00:28:46 +0800

    ProxyFactoryBeanAopTest

---
+++ src/main/java/bean/aop/ProxyFactoryBeanAopTest.java
@@ -0,0 +1,30 @@
+package bean.aop;
+
+import model.aop.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author wuxinxin
+ *
+ * ProxyFactoryBean方式配置生成代理
+ *
+ * 1.ProxyFactoryBean实际上是一个BeanFactory类型，和其他BeanFactory没什么两样
+ * 2.通过BeanFactory创建的bean都是独立存储的，存在容器的factoryBeanObjectCache 而不是像普通的bean一样存在singletonObjects
+ * 3.ProxyFactoryBean只不过这个BeanFactory的getObject实现是去创建一个代理对象
+ * 4.ProxyFactoryBean是个单例工厂，所以只会创建一个指定类型的对象的代理对象；
+ *   如果还需要别的对象的代理对象，那么还要再加入一个ProxyFactoryBean的工厂，然后创建别的类型的代理对象
+ * 5.ProxyFactoryBean被生成的代理在创建就已经固定好了，每次调用不同的方法都会代理，是类级别的
+ */
+public class ProxyFactoryBeanAopTest {
+
+    public static  void main(String[] args) {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("aop/ProxyFactoryBeanAopTest.xml");
+
+        UserService userService = (UserService)classPathXmlApplicationContext.getBean("userServiceProxy");
+
+
+        userService.createUser("aa");
+
+        userService.queryUser();
+    }
+}
